eca053c8437dde553b21cf63f3de95f644d1e994
singlasonu2020 <ssgroup.net2022@gmail.com>
2022-11-05 23:59:40 +0530

    Spring-2 final 5/11/2022

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 const port = 8000;
 const fs = require("fs");
 var cors = require('cors');
+var Jwt = require('jsonwebtoken');
+const { verify } = require('crypto');
+var jwtKey = "Key-NewsApp";
+
+
 app.use(cors());
 
 var connection = mysql.createConnection({
@@ -28,18 +33,25 @@ app.get("/check_login", (req, res) => {
 
     const data = req.query;
     connection.query(`select UserId from userdetails where MailId = "${data.id}" && Passsword = "${data.password}"; `, (err, result) => {
-        if (err)
-        {
-            console.log(err);
-            res.send({ status: 401, UserId: "" });
+        if (err) {
+            res.status(401).send();
         }
 
         else {
             if (result.length != 0) {
-                res.send({ status: 200, UserId: result[0].UserId });
+                var UserId = result[0].UserId;
+                Jwt.sign({ UserId }, jwtKey, (e, token) => {
+                    if (e) {
+                        res.status(401).send();
+                    }
+
+                    else {
+                        res.send({ UserId: UserId, token: token });
+                    }
+                })
             }
             else {
-                res.send({ status: 401, UserId: "" });
+                res.status(401).send();
             }
         }
 
@@ -51,33 +63,45 @@ app.get("/check_google_login", (req, res) => {
 
     const data = req.query;
     connection.query(`select MailId from userdetails where UserId="GI-${data.googleId}"; `, (err, result) => {
-        if (err)
-        {
-            console.log(err);
-            res.send({ status: 401, UserId: "" });
+        if (err) {
+            res.status(401).send();
         }
 
         else {
+            var UserId = `GI-${data.googleId}`;
             if (result.length == 0) {
 
-                connection.query(`INSERT INTO userdetails (FirstName, MailId , Passsword , UserId) VALUES ('${data.name}', '${data.email}', 'Google Login', "GI-${data.googleId}");`,(errr,resu)=>{
+                connection.query(`INSERT INTO userdetails (FirstName, MailId , Passsword , UserId,profile_url) VALUES ('${data.name}', '${data.email}', 'Google Login', "GI-${data.googleId}","${data.imageUrl}");`, (errr, resu) => {
 
                     if (errr) {
-                        console.log(errr);
-                        res.send({ status: 401, UserId: "" });
-            
+                        res.status(401).send();
+
                     }
-            
+
                     else {
-                        console.log("hello g");
-                        res.send({ status: 200, UserId: data.googleId });
+                        Jwt.sign({ UserId }, jwtKey, (e, token) => {
+                            if (e) {
+                                res.status(401).send();
+                            }
+
+                            else {
+                                res.send({ status: 200, UserId: UserId, token: token });
+                            }
+                        })
                     }
                 })
 
             }
             else {
-                console.log("hello hi");
-                res.send({ status: 200, UserId: data.googleId });
+                Jwt.sign({ UserId }, jwtKey, (e, token) => {
+                    if (e) {
+                        res.status(401).send();
+                    }
+
+                    else {
+                        res.send({ UserId: UserId, token: token });
+                    }
+                })
             }
         }
 
@@ -91,19 +115,81 @@ app.get("/sign_up", (req, res) => {
 
     connection.query(`INSERT INTO userdetails (FirstName, MailId , Passsword , UserId) VALUES ('${data.FirstName}', '${data.MailId}', '${data.Password}', "NA-${data.MailId}");`, (err, result) => {
         if (err) {
-            console.log(err);
-            res.send({ status: 401 });
+            res.status(401).send();
 
         }
 
         else {
-            res.send({ status: 200 });
+            var UserId = `NA-${data.MailId}`;
+            Jwt.sign({ UserId }, jwtKey, (e, token) => {
+                if (e) {
+                    res.status(401).send();
+                }
+
+                else {
+                    res.send({UserId: UserId, token: token });
+                }
+            })
         }
 
     })
 })
 
+app.get("/user_name",VerifyToken,(req,res)=>{
+    const data = req.query;
+    const UserId = data.UserId;
+    connection.query(`select FirstName,profile_url from userdetails where UserId = "${UserId}"; `, (err, result) => {
+        if(err)
+        {
+            res.status(401).send();   
+        }
+        else
+        {
+            if(result.length==0)
+            {
+                res.status(401).send();   
 
+            }
+
+            else
+            {
+                res.send(result[0]);   
+
+            }
+        }
+    })
+
+
+
+})
+
+
+
+
+function VerifyToken(req,res,next)
+{
+    let token = req.headers['authorization'];
+    if(token)
+    {
+        Jwt.verify(token,jwtKey,(err,vaild)=>{
+            if(err)
+            {
+                res.status(401).send({result: "Please Add Correct Token With Header"});
+
+            }
+            else
+            {
+                next();
+            }
+
+        })
+    }
+
+    else
+    {
+        res.status(403).send({result: "Please Add Token With Header"});
+    }
+}
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
