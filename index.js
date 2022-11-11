dd0963dde440856e0a8e21cb26f39187f82c9fa5
singlasonu2020 <ssgroup.net2022@gmail.com>
2022-11-11 23:09:32 +0530

    Sprint-2 finally

--- index.js
+++ index.js
@@ -114,66 +114,58 @@ app.get("/sign_up", (req, res) => {
     const data = req.query;
 
 
-    connection.query(`select UserId from userdetails where MailId = "${data.MailId}";` , (err,result)=>{
-        if(err)
-        {
+    connection.query(`select UserId from userdetails where MailId = "${data.MailId}";`, (err, result) => {
+        if (err) {
             res.status(401).send();
         }
-        else
-        {
-            if(result.length==0)
-            {
+        else {
+            if (result.length == 0) {
                 connection.query(`INSERT INTO userdetails (FirstName, MailId , Passsword , UserId) VALUES ('${data.FirstName}', '${data.MailId}', '${data.Password}', "NA-${data.MailId}");`, (errr, results) => {
                     if (errr) {
                         res.status(401).send();
-            
+
                     }
-            
+
                     else {
                         var UserId = `NA-${data.MailId}`;
                         Jwt.sign({ UserId }, jwtKey, (e, token) => {
                             if (e) {
                                 res.status(401).send();
                             }
-            
+
                             else {
-                                res.send({UserId: UserId, token: token });
+                                res.send({ UserId: UserId, token: token });
                             }
                         })
                     }
-            
+
                 })
 
             }
 
-            else
-            {
-                res.send({UserId: "Email Id Used", token: "" });
+            else {
+                res.send({ UserId: "Email Id Used", token: "" });
             }
         }
     })
-   
+
 })
 
-app.get("/user_name",VerifyToken,(req,res)=>{
+app.get("/user_name", VerifyToken, (req, res) => {
     const data = req.query;
     const UserId = data.UserId;
-    connection.query(`select FirstName,profile_url from userdetails where UserId = "${UserId}"; `, (err, result) => {
-        if(err)
-        {
-            res.status(401).send();   
+    connection.query(`select * from userdetails where UserId = "${UserId}"; `, (err, result) => {
+        if (err) {
+            res.status(401).send();
         }
-        else
-        {
-            if(result.length==0)
-            {
-                res.status(401).send();   
+        else {
+            if (result.length == 0) {
+                res.status(401).send();
 
             }
 
-            else
-            {
-                res.send(result[0]);   
+            else {
+                res.send(result[0]);
 
             }
         }
@@ -184,30 +176,106 @@ app.get("/user_name",VerifyToken,(req,res)=>{
 })
 
 
+app.get("/edit_user_data", VerifyToken, (req, res) => {
+
+    const data = req.query;
+
+    connection.query(`select UserId from userdetails where MailId = "${data.mail}" and UserId != "${data.UserId}";`, (err, result) => {
+        if (err) {
+            res.status(401).send();
+        }
+        else {
+            if (result.length == 0) {
+                if (data.profile_url == undefined) {
+                    connection.query(`UPDATE userdetails SET FirstName="${data.name}",MailId="${data.mail}" WHERE UserId="${data.UserId}";`, (errr, results) => {
+                        if (errr) {
+
+                            res.status(401).send();
+
+                        }
+
+                        else {
+                            res.send({ message: "successfully" })
+                        }
+
+                    })
+
+                }
+                else {
+                    connection.query(`UPDATE userdetails SET FirstName="${data.name}",MailId="${data.mail}",profile_url="${data.profile_url}" WHERE UserId="${data.UserId}";`, (errr, results) => {
+                        if (errr) {
+
+                            res.status(401).send();
+
+                        }
+
+                        else {
+                            res.send({ message: "successfully" })
+                        }
+
+                    })
+
+                }
 
 
-function VerifyToken(req,res,next)
-{
-    let token = req.headers['authorization'];
-    if(token)
-    {
-        Jwt.verify(token,jwtKey,(err,vaild)=>{
-            if(err)
+            }
+
+            else {
+                res.send({ message: "Email Id Used" });
+            }
+        }
+    })
+})
+
+app.get("/edit_password", (req, res) => {
+    const data = req.query;
+    if (data.UserId.split("-")[0] == "GI") {
+        connection.query(`UPDATE userdetails SET Passsword="${data.NewPassword}" WHERE UserId="${data.UserId}";`, (errr, results) => {
+            if(errr)
             {
-                res.status(401).send({result: "Please Add Correct Token With Header"});
-
+                res.status(401).send();
             }
             else
             {
+                res.send({ message: "successfully" })
+            }
+        });
+
+    }
+    else {
+
+        connection.query(`UPDATE userdetails SET Passsword="${data.NewPassword}" WHERE UserId="${data.UserId}" and Passsword="${data.OldPassword}";`, (errr, results) => {
+            if(errr)
+            {
+                res.status(401).send();
+            }
+            else
+            {
+                res.send({ changedRows: results.changedRows })
+            }
+        });
+
+    }
+})
+
+
+function VerifyToken(req, res, next) {
+    let token = req.headers['authorization'];
+    if (token) {
+        Jwt.verify(token, jwtKey, (err, vaild) => {
+            if (err) {
+                res.status(401).send({ result: "Please Add Correct Token With Header" });
+
+            }
+            else {
                 next();
             }
 
         })
     }
 
-    else
-    {
-        res.status(403).send({result: "Please Add Token With Header"});
+    else {
+        res.status(403).send({ result: "Please Add Token With Header" });
     }
 }
 
